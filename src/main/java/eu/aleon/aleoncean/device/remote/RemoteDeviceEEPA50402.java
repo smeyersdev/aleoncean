9d69f67b0c2df01420ee3f36f3279b55f574534b
Stephan Meyer <smeyersdev@gmail.com>
2015-01-05 00:25:08 +0100

    Refactor copy-paste programming for EEP A50402

--- src/main/java/eu/aleon/aleoncean/device/remote/RemoteDeviceEEPA50402.java
+++ src/main/java/eu/aleon/aleoncean/device/remote/RemoteDeviceEEPA50402.java
@@ -11,7 +11,7 @@ import eu.aleon.aleoncean.device.StandardDevice;
 import eu.aleon.aleoncean.packet.EnOceanId;
 import eu.aleon.aleoncean.packet.RadioPacket;
 import eu.aleon.aleoncean.packet.radio.RadioPacket4BS;
-import eu.aleon.aleoncean.packet.radio.userdata.UserDataEEPA50401;
+import eu.aleon.aleoncean.packet.radio.userdata.UserDataEEPA50402;
 import eu.aleon.aleoncean.packet.radio.userdata.UserDataScaleValueException;
 import eu.aleon.aleoncean.rxtx.ESP3Connector;
 
@@ -19,37 +19,14 @@ import eu.aleon.aleoncean.rxtx.ESP3Connector;
  *
  * @author Stephan Meyer <smeyersdev@gmail.com>
  */
-public class RemoteDeviceEEPA50402 extends StandardDevice implements RemoteDevice {
+public class RemoteDeviceEEPA50402 extends RemoteDeviceEEPA50401 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RemoteDeviceEEPA50402.class);
-
-    private Double humidity;
-    private Double temperature;
-
+  
     public RemoteDeviceEEPA50402(final ESP3Connector conn,
                                  final EnOceanId addressRemote,
                                  final EnOceanId addressLocal) {
         super(conn, addressRemote, addressLocal);
-    }
-
-    public Double getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(final DeviceParameterUpdatedInitiation initiation, final Double humidity) {
-        final Double oldHumidity = this.humidity;
-        this.humidity = humidity;
-        fireParameterChanged(DeviceParameter.HUMIDITY_PERCENT, initiation, oldHumidity, humidity);
-    }
-
-    public Double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(final DeviceParameterUpdatedInitiation initiation, final Double temperature) {
-        final Double oldTemperature = this.temperature;
-        this.temperature = temperature;
-        fireParameterChanged(DeviceParameter.TEMPERATURE_CELSIUS, initiation, oldTemperature, temperature);
     }
 
     private void parseRadioPacket4BS(final RadioPacket4BS packet) {
@@ -58,7 +35,7 @@ public class RemoteDeviceEEPA50402 extends StandardDevice implements RemoteDevic
             return;
         }
 
-        final UserDataEEPA50401 userData = new UserDataEEPA50401(packet.getUserDataRaw());
+        final UserDataEEPA50402 userData = new UserDataEEPA50402(packet.getUserDataRaw());
         try {
             setHumidity(DeviceParameterUpdatedInitiation.RADIO_PACKET, userData.getHumidity());
         } catch (final UserDataScaleValueException ex) {
@@ -80,29 +57,4 @@ public class RemoteDeviceEEPA50402 extends StandardDevice implements RemoteDevic
             LOGGER.warn("Don't know how to handle radio choice 0x%02X.", packet.getChoice());
         }
     }
-
-    @Override
-    protected void fillParameters(final Set<DeviceParameter> params) {
-        params.add(DeviceParameter.HUMIDITY_PERCENT);
-        params.add(DeviceParameter.TEMPERATURE_CELSIUS);
-    }
-
-    @Override
-    public Object getByParameter(final DeviceParameter parameter) throws IllegalDeviceParameterException {
-        switch (parameter) {
-            case HUMIDITY_PERCENT:
-                return getHumidity();
-            case TEMPERATURE_CELSIUS:
-                return getTemperature();
-            default:
-                return super.getByParameter(parameter);
-        }
-    }
-
-    @Override
-    public void setByParameter(final DeviceParameter parameter, final Object value) throws IllegalDeviceParameterException {
-        assert DeviceParameter.getSupportedClass(parameter).isAssignableFrom(value.getClass());
-        super.setByParameter(parameter, value);
-    }
-
 }
